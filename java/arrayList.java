df4a7bfbb80db9f2e83a7368aa43e45b59351ba0
tushar-ntiave <tushar.nitave@gmail.com>
2020-01-09 17:18:01 -0600

    array list

---
+++ java/arrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class arrayList{
+    public static void main(String args[]){
+        ArrayList<String> array = new ArrayList<String>();
+        array.add("Tushar");
+        array.add("Nitave");
+        System.out.println(array.get(0)+" "+array.get(1));
+    }
+}
